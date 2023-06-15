031f391ea60d017d9d6e9b6fb7a9afcae43bc4cc
almustanir <olajidemubarak561@gmail.com>
2023-06-15 11:21:05 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+import {disableReactDevtools} from "@fvilers/disable-react-devtools"
+
+if(process.env.NODE_ENV === "production")disableReactDevtools();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
